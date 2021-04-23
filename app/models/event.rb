eaa932720903f7d5558e2f805b52c00aff8a748a
ukrmaxim <ukrmaxim@protonmail.com>
2021-04-23 21:35:44 +0300

    Config PostgreSql

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
+  belongs_to :user
+
+  validates :user, :address, :datetime, presence: true
   validates :title, presence: true, length: {maximum: 255}
-  validates :address, presence: true
-  validates :datetime, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  USERNAME_REGEXP = /\A\w+\z/
+  USEREMAIL_REGEXP = /\A[\w.]+@[a-zа-я\d]+\.[a-zа-я]+\z/
+
+  has_many :events
+
+  validates :name, presence: true, length: { maximum: 40 }, format: { with: USERNAME_REGEXP }
+  validates :email, presence: true, uniqueness: true, format: { with: USEREMAIL_REGEXP }
 end
