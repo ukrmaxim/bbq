8d705d9361557d82b3f8181932bfe65d73ddad3b
ukrmaxim <ukrmaxim@protonmail.com>
2021-04-27 23:24:49 +0300

    Delete event

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   USERNAME_REGEXP = /\A[a-zA-ZА-Яа-я\d]+\z/
+
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_many :events
 
