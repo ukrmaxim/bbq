f990b3db10a4dee73cd060a651312f3f353dcade
ukrmaxim <ukrmaxim@protonmail.com>
2021-05-25 20:19:08 +0300

    Replace nested conditional with Guard Clauses in yandexmap.js

--- app/frontend/packs/yandexmap.js
+++ app/frontend/packs/yandexmap.js
@@ -1,38 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     ymaps.ready(init);
     let map = document.getElementById('map')
-
     function init(){
-        if (!map) {
-            return
-        } else {
-            let address = map.getAttribute('data-address');
+        if (!map) { return }
+        let address = map.getAttribute('data-address');
+        let myMap = new ymaps.Map("map", {
+           center: [49.9935, 36.23038],
+           zoom: 10
+        });
+        myGeocoder = ymaps.geocode(address);
+        myGeocoder.then(
+            function (res) {
+                coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+                myMap.geoObjects.add(
+                    new ymaps.Placemark(
+                        coordinates,
+                        {iconContent: address},
+                        {preset: 'islands#blueStretchyIcon'}
+                    )
+                );
+                myMap.setCenter(coordinates);
+                myMap.setZoom(15);
+            },
+            function (err) {
+                alert('Ошибка при определении местоположения');
+            }
+        );
 
-            let myMap = new ymaps.Map("map", {
-                center: [49.9935, 36.23038],
-                zoom: 10
-            });
-
-            myGeocoder = ymaps.geocode(address);
-
-            myGeocoder.then(
-                function (res) {
-                    coordinates = res.geoObjects.get(0).geometry.getCoordinates();
-
-                    myMap.geoObjects.add(
-                        new ymaps.Placemark(
-                            coordinates,
-                            {iconContent: address},
-                            {preset: 'islands#blueStretchyIcon'}
-                        )
-                    );
-
-                    myMap.setCenter(coordinates);
-                    myMap.setZoom(15);
-                }, function (err) {
-                    alert('Ошибка при определении местоположения');
-                }
-            );
-        }
     }
 })
