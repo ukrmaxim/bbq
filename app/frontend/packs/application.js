4af402330eaf1ad33436393c51d8ef1a66ec035a
ukrmaxim <ukrmaxim@protonmail.com>
2021-05-26 11:45:49 +0300

    Add ekko-lightbox gallery

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,17 +1,13 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/frontend and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 
-Rails.start()
-
-
-import '../styles/application.scss'
 import 'bootstrap/dist/js/bootstrap'
+import '../scripts/ekko_lightbox'
+import '../styles/application.scss'
+import 'ekko-lightbox'
 import "@fortawesome/fontawesome-free/css/all"
 import './yandexmap'
+
+Rails.start()
 
 const images = require.context('../images', true)
 
