067d4f2c904636ab8e0a0026f6305283e1ce00fc
ukrmaxim <ukrmaxim@protonmail.com>
2021-07-19 15:40:07 +0300

    Refactor EventMailer & EventMailerJob

--- app/jobs/event_mailer_job.rb
+++ app/jobs/event_mailer_job.rb
@@ -2,22 +2,25 @@ class EventMailerJob < ApplicationJob
   queue_as :mailer
 
   def perform(record)
-    event = record.event
-
     case record
     when Comment
-      all_emails = event.subscriptions.map(&:user_email) + [event.user.email] - [record.user&.email]
-      all_emails.each do |mail|
-        EventMailer.comment(event, record, mail).deliver_later
+      all_emails(record).each do |mail|
+        EventMailer.comment(record, mail).deliver_later
       end
     when Photo
-      all_emails = event.subscriptions.map(&:user_email) + [event.user.email] - [record.user&.email]
-
-      all_emails.each do |mail|
-        EventMailer.photo(event, record, mail).deliver_later
+      all_emails(record).each do |mail|
+        EventMailer.photo(record, mail).deliver_later
       end
     when Subscription
-      EventMailer.subscription(event, record).deliver_later
+      EventMailer.subscription(record).deliver_later
     end
+  end
+
+  private
+
+  def all_emails(record)
+    event = record.event
+
+    event.subscriptions.map(&:user_email) + [event.user.email] - [record.user&.email]
   end
 end
