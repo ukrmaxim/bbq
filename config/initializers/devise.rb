4f63e7ef03513560a5c1065e73bcb054583a360c
ukrmaxim <ukrmaxim@protonmail.com>
2021-08-04 13:52:58 +0300

    Fix smtp auth error

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,11 +271,6 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook,
-                  Rails.application.credentials[Rails.env.to_sym][:facebook][:omniauth_id],
-                  Rails.application.credentials[Rails.env.to_sym][:facebook][:omniauth_secret],
-                  scope: 'email, public_profile'
-
   config.omniauth :github,
                   Rails.application.credentials[Rails.env.to_sym][:github][:omniauth_id],
                   Rails.application.credentials[Rails.env.to_sym][:github][:omniauth_secret]
